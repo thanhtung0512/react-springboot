1d01664b8c06be2766cd07b569ccf87586647916
Thanh Tung <21020399@vnu,edu.vn>
2023-08-15 23:58:13 +0700

    pass as children text

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,12 @@
 import { Fragment } from "react";
 import Message from "./Message";
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  const items = ["Paris", "London", "Moscow", "Luxembourg", "France"];
-  const colors = ["Black", "White", "Blue"];
-  const handleSelectItem = (item: string) => {
-    console.log(item + "Pass function via props");
-  }
   return (
     <Fragment>
-      <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem} />
-
+      <Alert>Hello World</Alert>
     </Fragment>
   );
 }
---
+++ client/src/components/Alert.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  children: string;
+}
+
+const Alert = ({ children }: Props) => {
+  return <div className="alert alert-primary">{children}</div>;
+};
+
+export default Alert;

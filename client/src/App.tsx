7048503841be755e431176c06887f6b50796d182
Thanh Tung <21020399@vnu,edu.vn>
2023-08-16 00:37:13 +0700

    button exercise

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,19 @@ import { Fragment } from "react";
 import Message from "./Message";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
-
+import Button from "./components/Button";
 function App() {
+  const hanleOnClick = () => {
+    console.log("Click this button");
+  };
+
   return (
     <Fragment>
       <Alert>
         Hello <span>World</span>
       </Alert>
+
+      <Button color="secondary" onClick={hanleOnClick}>MyButton</Button>
     </Fragment>
   );
 }
---
+++ client/src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {
+  children: string;
+  color?: 'primary' | 'secondary' | 'danger'; // color is optional property
+  onClick: () => void;
+}
+
+const Button = ({ children, color = 'primary', onClick }: Props) => {
+  return (
+    <button type="button" className={"btn btn-" +  color } onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;

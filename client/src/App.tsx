249ec97e8eda8d89f4a69c7a3592b1f35337451d
Thanh Tung <21020399@vnu,edu.vn>
2023-08-19 22:45:51 +0700

    alert practice

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,33 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Message from "./Message";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 function App() {
+  const [isClickButton, setIsClickButton] = useState(false);
+  const [isShow,setIsShow] = useState(false);
   const hanleOnClick = () => {
     console.log("Click this button");
+    setIsClickButton(true);
+    if(isShow === false) {
+      setIsShow(true);
+    }
+    else {
+      setIsShow(false);
+    }
   };
 
   return (
     <Fragment>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      {isClickButton === true && isShow === true ? (
+        <Alert>
+          Hello <span>World</span>
+        </Alert>
+      ) : null}
 
-      <Button color="secondary" onClick={hanleOnClick}>MyButton</Button>
+      <Button color="secondary" onClick={hanleOnClick}>
+        MyButton
+      </Button>
     </Fragment>
   );
 }

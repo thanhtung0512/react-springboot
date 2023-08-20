c77aaa2aa72d5c89c058acac7e2a12fa9b7d5e91
Thanh Tung <21020399@vnu,edu.vn>
2023-08-20 16:28:05 +0700

    procedure immer not copy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Message from "./components/Message";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import produce from "immer";
 function App() {
   const [drink, setDrink] = useState({
     title: "Americano",
@@ -19,6 +20,11 @@ function App() {
 
   const [tags, setTags] = useState(["happy", "cheerful"]);
 
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: "false" },
+    { id: 2, title: "Bug 2", fixed: "false" },
+  ]);
+
   const handleClick = () => {
     // setDrink({ ...drink, price: 6 });
     // setCustomer({
@@ -26,19 +32,32 @@ function App() {
     //   address: { ...customer.address, zipCode: 94112 },
     // });
 
-    setTags([...tags, "exciting"]);
+    // setTags([...tags, "exciting"]);
 
-    //Remove
-    setTags(tags.filter((tag) => tag !== "happy"));
+    // //Remove
+    // setTags(tags.filter((tag) => tag !== "happy"));
 
-    //Update
-    setTags(tags.map((tag) => (tag === "happy" ? "hapiness" : tag)));
+    // //Update
+    // setTags(tags.map((tag) => (tag === "happy" ? "hapiness" : tag)));
+
+    //setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: "true" } : bug)));
+
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) {
+          bug.fixed = "true";
+        }
+      })
+    );
   };
 
   return (
     <Fragment>
-      {tags.map((tag) => (
-        <div>{tag}</div>
+      {bugs.map((bug) => (
+        <div>
+          {bug.id} {bug.title} {bug.fixed}
+        </div>
       ))}
       <button onClick={handleClick}>Click me</button>
     </Fragment>
